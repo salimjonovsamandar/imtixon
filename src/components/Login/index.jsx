21fa838d77294de62bf21b7e8b69404d02be65e4
salimjonovsamandar <salimjonovsamandarbek777@gmail.com>
2024-02-12 21:17:14 +0500

    Bajarilyabdi

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,35 +1,98 @@
-import React from 'react'
-import male from '../Login/male.png'
-import Logo from '../Login/Logo.svg'
-import styles from './index.module.css'
+import Logo from "../Login/Logo.svg";
+import React from "react";
+import male from "../Login/male.png";
+import styles from "./index.module.css";
+import { useRef } from "react";
+import axios from "axios";
 
-function index() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.text}>
-                <img className={styles.logo} src={Logo} alt="" />
-                <h1 className={styles.Title}>Xush kelibsiz!</h1>
-                <p className={styles.desc}>Login parolingizni kiritib o'z kabinetingizga kiring.</p>
+function Login() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passWordRef = useRef();
 
-                <div className={styles.name}>
-                    <label className={styles.label}>Login</label>
-                    <input type="text" required="" name="text" className={styles.input} />
-                </div>
+  function validateName(name) {
+    if (name.trim() === "") {
+      alert("Login kiritilishi shart");
+      nameRef.current.focus();
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-                <div className={styles.name}>
-                    <label className={styles.label}>Parol</label>
-                    <input type="password" required="" className={styles.input} />
-                </div>
+  function validatePassword(password) {
+    if (password.trim() === "") {
+      alert("Parol bo'sh bo'lishi mumkin emas");
+      passWordRef.current.focus();
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-                <button className={styles.button}>Kirish</button>
-                <h6 className={styles.head}>Copyright © 2024 Vim kompaniyasi</h6>
-            </div>
+  function hendleSubmit() {
+    if (
+      validateName(nameRef.current.value) &&
+      validatePassword(passWordRef.current.value)
+    ) {
+      let data = {
+        username: `${nameRef.current.value}`,
+        email: `${emailRef.current.value}`,
+        password: `${passWordRef.current.value}`,
+      };
+      axios
+        .post("https://auth-rg69.onrender.com/api/auth/signup", data)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("Ma'lumotlarni olishda xatolik yuz berdi:", error);
+        });
+    }
+  }
 
-            <div className="img">
-                <img className={styles.img} src={male} width={600} height={400} />
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>
+        <img className={styles.logo} src={Logo} alt="" />
+        <h1 className={styles.Title}>Xush kelibsiz!</h1>
+        <p className={styles.desc}>
+          Login parolingizni kiritib o'z kabinetingizga kiring.
+        </p>
+
+        <div className={styles.name}>
+          <label className={styles.label}>Login</label>
+          <input
+            ref={nameRef}
+            type="text"
+            required=""
+            name="text"
+            className={styles.input}
+          />
         </div>
-    )
+
+        <div className={styles.name}>
+          <label className={styles.label}>Parol</label>
+          <input
+            ref={passWordRef}
+            type="password"
+            required=""
+            name="text"
+            className={styles.input}
+          />
+        </div>
+
+        <button onClick={hendleSubmit} className={styles.button}>
+          Kirish
+        </button>
+        <h6 className={styles.head}>Copyright © 2024 Vim kompaniyasi</h6>
+      </div>
+
+      <div className="img">
+        <img className={styles.img} src={male} width={600} height={400} />
+      </div>
+    </div>
+  );
 }
 
-export default index
+export default Login;
--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,76 +1,130 @@
-import React from 'react'
-import male from '../Login/male.png'
-import Logo from '../Register/Logo.svg'
+import React from "react";
+import { useRef } from "react";
+import axios from "axios";
+import Logo from '../Login/Logo.svg'
+import male from "../Login/male.png";
+import styles from "./index.module.css";
+
+const validatemail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
+
+function Login() {
+    const nameRef = useRef();
+
+    const passWordRef = useRef();
+    const passRef = useRef();
+
+    function validateName(name) {
+        if (name.trim() === "") {
+            alert("Login kiritilishi shart")
+            nameRef.current.focus();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function validatePassword(password) {
+        if (password.trim() === "") {
+            alert("Parol bo'sh bo'lishi mumkin emas")
+            passWordRef.current.focus();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function validatePassword() {
+        if (passWordRef.current.value != passRef.current.value) {
+            alert("Parol bir xil bo'lishi kerak")
+            passWordRef.current.focus();
+            return false;
+        } else {
+            return true;
+        }
+    }
 
 
-function index() {
+    function hendleSubmit() {
+        if (
+            validateName(nameRef.current.value) &&
+            validatePassword(passWordRef.current.value)
+        ) {
+            let data = {
+                username: `${nameRef.current.value}`,
+                email: `${emailRef.current.value}`,
+                password: `${passWordRef.current.value}`
+            }
+            axios
+                .post("https://auth-rg69.onrender.com/api/auth/signup", data)
+                .then((response) => {
+                    console.log(response.data);
+                })
+                .catch((error) => {
+                    console.error("Ma'lumotlarni olishda xatolik yuz berdi:", error);
+                });
+        }
+    }
+
     return (
-        <section className="vh-100  bg-#441111" >
-            <div className="container h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col-lg-12 col-xl-11">
-                        <div className="card-body row justify-content-between">
-                            <div className="row justify-content-between ">
+        <div className={styles.container}>
+            <div className={styles.text}>
+                <img className={styles.logo} src={Logo} alt="" />
+                <h1 className={styles.Title}>Xush kelibsiz!</h1>
+                <p className={styles.desc}>
+                    Login parolingizni kiritib o'z kabinetingizga kiring.
+                </p>
 
-                                <div className="col-xl-5 ml-2">
-                                    <img className='align-items-center d-inline-block ml-5' src={Logo} alt="" />
-                                    <h1 className="text-center fw-bold mb-3 mt-4">Register</h1>
-                                    <form>
-                                        <div className="d-flex justify-content-center align-items-center mb-4">
-                                            {/* <i className="fas fa-user fa-lg me-3 fa-fw"></i> */}
-                                            <div className="w-75 mb-0">
-                                                <label className="form-label" >Your Name</label>
-                                                <input type="text" className="form-control " />
-                                            </div>
-                                        </div>
-
-                                        <div className="d-flex align-items-center justify-content-center mb-4">
-                                            {/* <i className="fas fa-envelope fa-lg me-3 fa-fw"></i> */}
-                                            <div className="w-75 mb-0">
-                                                <label className="form-label" >Your Email</label>
-                                                <input type="email" className="form-control" />
-                                            </div>
-                                        </div>
-
-                                        <div className="d-flex  justify-content-center align-items-center mb-4">
-                                            {/* <i className="fas fa-lock fa-lg me-3 fa-fw"></i> */}
-                                            <div className="w-75 mb-0">
-                                                <label className="form-label">Password</label>
-                                                <input type="password" className="form-control" />
-                                            </div>
-                                        </div>
-
-                                        <div className="d-flex justify-content-center align-items-center mb-4 ">
-                                            {/* <i className="fas fa-key fa-lg me-3 fa-fw"></i> */}
-                                            <div className="w-75 mb-0">
-                                                <label className="form-label" >Repeat your password</label>
-                                                <input type="password" className="form-control" />
-                                            </div>
-                                        </div>
-
-                                        <div className="d-flex justify-content-center mx- mb-3 mb-lg-4 mr-2">
-                                            <button type="button" className="btn btn-primary btn-lg w-75 mt-2">Register</button>
-                                        </div>
-
-                                    </form>
-
-                                </div>
-
-                                <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                                    <img src={male} alt="Sample image" className="img-fluid" />
-
-                                </div>
-
-                            </div>
-                        </div>
-
-                    </div>
+                <div className={styles.name}>
+                    <label className={styles.label}>Login</label>
+                    <input
+                        ref={nameRef}
+                        type="text"
+                        required=""
+                        name="text"
+                        className={styles.input}
+                    />
                 </div>
+
+                <div className={styles.name}>
+                    <label className={styles.label}>Parol</label>
+                    <input
+                        ref={passWordRef}
+                        type="password"
+                        required=""
+                        name="text"
+                        className={styles.input}
+                    />
+                </div>
+
+                <div className={styles.name}>
+                    <label className={styles.label}>Parol qayta kiriting</label>
+                    <input
+                        ref={passRef}
+                        type="password"
+                        required=""
+                        name="text"
+                        className={styles.input}
+                    />
+                </div>
+
+
+                <button onClick={hendleSubmit} className={styles.button}>
+                    Kirish
+                </button>
+                <h6 className={styles.head}>Copyright © 2024 Vim kompaniyasi</h6>
             </div>
-        </section>
 
-
-    )
+            <div className="img">
+                <img className={styles.img} src={male} width={600} height={400} />
+            </div>
+        </div>
+    );
 }
 
-export default index
+export default Login;
